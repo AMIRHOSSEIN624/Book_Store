22bbbae15fd6b309201966b3142c67f094621a65
amirhossein <amirhossein624@gmail.com>
2022-07-09 17:21:55 +0430

    create view comment

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
 from django.views import generic
+from .forms import CommentForm
 
 
 class BookList(generic.ListView):
@@ -12,5 +13,16 @@ class BookList(generic.ListView):
 
 def detail_page(request, pk):
     book = get_object_or_404(Book, pk=pk)
+    comment = book.comments.all()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.book = book
+            new_comment.user = request.user
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
 
-    return render(request, 'pages/detail_page.html', {'book': book})
+    return render(request, 'pages/detail_page.html', {'book': book, 'comments': comment,'comment_form':comment_form})
